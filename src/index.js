67f2964b49ff73171a01751a84b51330631b46ed
Phonekham <phonekham.dev@gmail.com>
2019-06-11 10:29:58 +0700

    class base component

--- src/index.js
+++ src/index.js
@@ -1,38 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import CommentDetail from "./CommentDetail";
-import ApprovalCard from "./ApprovalCard";
-import faker from "faker";
+import SeasonDisplay from "./SeasonDisplay";
+// import CommentDetail from "./CommentDetail";
+// import ApprovalCard from "./ApprovalCard";
+// import faker from "faker";
 
-const App = () => {
-  return (
-    <div className="ui container comments">
-      <ApprovalCard>
-        <CommentDetail
-          author="phonekham"
-          timeAgo="Today at 6:00"
-          content="Nice blog post"
-          avatar={faker.image.avatar()}
-        />
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail
-          author="phone"
-          timeAgo="Today at 6:00"
-          content="Nice blog love you"
-          avatar={faker.image.avatar()}
-        />
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail
-          author="test"
-          timeAgo="Today at 6:00"
-          content="Nice blog post haha"
-          avatar={faker.image.avatar()}
-        />
-      </ApprovalCard>
-    </div>
-  );
-};
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      err => console.log(err)
+    );
+    return <div>Ladtitude:</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
