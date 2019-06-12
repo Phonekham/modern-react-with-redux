00b014fd99fbb80ddeb3631ae68675091094ac6f
Phonekham <phonekham.dev@gmail.com>
2019-06-12 12:11:29 +0700

    axios  Viewing Request Results

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term); //involk callback in children
+    // console.log(term); //involk callback in children
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 4400daddffec65fd202ec21d290a530d8554bcdfd48f36131e657b7d77d89b81"
+      }
+    });
   }
   render() {
     return (
