197a7449830c3ad1cf3bc76b8f869c6415b9be5a
Phonekham <phonekham.dev@gmail.com>
2019-06-12 09:23:05 +0700

    Life cycle Methon

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,39 @@
 import React from "react";
+import "./SeasonDisplay.css";
 
-const SeasonDisplay = () => {
-  return <div>Season display</div>;
+const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
+  console.log(props.lat);
+
+  console.log(season);
+
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive icon ${iconName}`} />
+      <h1>{text}</h1>
+      <i className={`icon-right massive icon ${iconName}`} />
+    </div>
+  );
+};
+
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach",
+    iconName: "sun"
+  },
+  winter: {
+    text: "Burr It is chilly",
+    iconName: "snowflake"
+  }
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 // import CommentDetail from "./CommentDetail";
 // import ApprovalCard from "./ApprovalCard";
 // import faker from "faker";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lat: null,
-      errorMessage: ""
-    };
+  state = {
+    lat: null,
+    errorMessage: ""
+  };
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      err => {
-        this.setState({ errorMessage: err.message });
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
-  render() {
+  componentDidUpdate() {
+    console.log("my component was just update");
+  }
+
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div> Error: {this.state.errorMessage}</div>;
     }
     if (this.state.lat && !this.state.errorMessage) {
-      return <div> latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
-    return <div>loading</div>;
+    return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
