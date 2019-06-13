2704637dacda147f9fe992b8b8b96ef15d11b50e
Phonekham <phonekham.dev@gmail.com>
2019-06-13 10:32:06 +0700

    Scaffolding App

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,37 +1,9 @@
 import React from "react";
 
-class SearchBar extends React.Component {
-  state = {
-    term: ""
-  };
-  //   Event Handler
-  onInputChange(event) {
-    event.preventDefault();
-    console.log(event.target.value);
-  }
-
-  onFormSubmit = event => {
-    event.preventDefault();
-    // console.log(this.state.term);
-    this.props.onSubmit(this.state.term); //invoke callback in children
-  };
+class SeachBar extends React.Component {
   render() {
-    return (
-      <div className="ui segment">
-        <form className="ui form" onSubmit={this.onFormSubmit}>
-          <div className="field">
-            <label>Image Search</label>
-            <input
-              type="text"
-              value={this.state.term}
-              //   onChange={this.onInputChange}
-              onChange={e => this.setState({ term: e.target.value })}
-            />
-          </div>
-        </form>
-      </div>
-    );
+    return <div>SeachBar</div>;
   }
 }
 
-export default SearchBar;
+export default SeachBar;
