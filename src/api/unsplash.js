ea318ec0fb7807f22c2c2356e118319bfef2134a
Phonekham <phonekham.dev@gmail.com>
2019-06-14 10:12:38 +0700

    scaffold the structure

--- src/api/unsplash.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://api.unsplash.com",
-  headers: {
-    Authorization:
-      "Client-ID 4400daddffec65fd202ec21d290a530d8554bcdfd48f36131e657b7d77d89b81"
-  }
-});
