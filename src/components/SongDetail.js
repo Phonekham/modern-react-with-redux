5bac138dca0fdc7a6e2bd9180227ba1c3de84424
Phonekham <phonekham.dev@gmail.com>
2019-06-14 21:32:05 +0700

    Conditional rendering : display selected song detail

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const SongDetail = () => {
+const SongDetail = ({ song }) => {
+  if (!song) {
+    return <div>select a song</div>;
+  }
   return (
     <div>
-      <h1>songdetail</h1>
+      <h3>Details for</h3>
+      <p>
+        Title : {song.title} <br />
+        Duration : {song.duration}
+      </p>
     </div>
   );
 };
