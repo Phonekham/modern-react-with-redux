15549d52e8f752da09ee17eea904474229fd05c1
Phonekham <phonekham.dev@gmail.com>
2019-06-15 08:57:25 +0700

    fetchPost actionCreator

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions";
 
 class PostList extends React.Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
   render() {
     return <div>post list</div>;
   }
 }
 
-export default PostList;
+export default connect(
+  null /** state mapstateToProps*/,
+  { fetchPosts } /** action creator*/
+)(PostList);
