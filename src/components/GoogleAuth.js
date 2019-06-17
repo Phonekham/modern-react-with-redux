8425e44551f195c68d196b28ee2ed882bc60b8e3
Phonekham <phonekham.dev@gmail.com>
2019-06-17 17:51:34 +0700

    OnSignIn Out

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -25,14 +25,14 @@ class GoogleAuth extends React.Component {
       return null;
     } else if (this.state.isSignedIn) {
       return (
-        <button className="ui google button red">
+        <button className="ui google button red" onClick={this.onSignOut}>
           <i className="google icon" />
           Sign Out
         </button>
       );
     } else {
       return (
-        <button className="ui google button red">
+        <button className="ui google button red" onClick={this.onSignIn}>
           <i className="google icon" />
           Sign In with Google
         </button>
@@ -42,6 +42,14 @@ class GoogleAuth extends React.Component {
 
   onAuthChange = () => {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
   };
 
   render() {
