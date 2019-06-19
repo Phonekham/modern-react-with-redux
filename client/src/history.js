2555a964f45050b17de0d35fb75e88b45d668586
Phonekham <phonekham.dev@gmail.com>
2019-06-19 17:14:42 +0700

    20. Creating a Browser History Object

---
+++ client/src/history.js
@@ -0,0 +1,4 @@
+// import createHistory from "history/createBrowserHistory";  this code from tutorial
+import { createBrowserHistory } from "history"; //code from google
+
+export default createBrowserHistory();
