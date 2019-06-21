ebb2a56726bb3158dba1b32d8f168f7192e2efb6
Phonekham <phonekham.dev@gmail.com>
2019-06-21 14:20:04 +0700

    8. Implementing FLV JS

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,8 +1,14 @@
 import React from "react";
+import flv from "flv";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super();
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
@@ -14,6 +20,7 @@ class StreamShow extends React.Component {
     const { title, description } = this.props.stream;
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls />
         <h2>{title}</h2>
         <h2>{description}</h2>
       </div>
